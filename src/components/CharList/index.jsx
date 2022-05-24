960d6ba48d44a2d202f36873fe6d2bf60387836e
Thiago Oliveira <thiago.oliveira_@outlook.com>
2022-05-24 18:34:48 -0300

    feature: create-charCard component

--- src/components/CharList/index.jsx
+++ src/components/CharList/index.jsx
@@ -1,26 +1,32 @@
-import { useEffect } from "react";
 import { useMarvel } from "../../context";
 
 export const CharList = () => {
-  const { characters, GetCharacters } = useMarvel();
-  useEffect(() => {
-    GetCharacters();
-  }, []);
-  console.log(characters);
+  const { characters } = useMarvel();
 
   return (
-    <>
-      <h1>
-        Characthers
-        {/* {characters &
-          characters.map((info) => {
-            return (
-              <>
-                <p>{info.name}</p>;
-              </>
-            );
-          })} */}
-      </h1>
-    </>
+    <div>
+      <p>Lista de Personagens</p>
+      <h1>Personagens</h1>
+      {characters.map((info) => {
+        return (
+          <div>
+            <img alt="" src={`${info.thumbnail.path}.jpg`}></img>
+            <p>{info.name}</p>
+            <div>
+              <h4>Quadrinhos</h4>
+              <ol>
+                {info.comics.items.map((item) => {
+                  return (
+                    <>
+                      <li>{item.name}</li>
+                    </>
+                  );
+                })}
+              </ol>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -6,35 +6,42 @@ export const MarvelContext = createContext();
 export const MarvelProvider = ({ children }) => {
   const [hash, setHash] = useState("");
   const [timeStamp, setTimeStamp] = useState("");
-  const apiKey = "b20c16f1ac80cb70cecd2ee43e396aaa";
+  const apiKey = "7fda74fc35c2561abc4a43fc02eda15e";
   const [comics, setComics] = useState({});
   const [characters, setCharacters] = useState({});
 
   useEffect(() => {
     let md5 = require("md5");
-
     const generateStamp = Math.floor(Date.now() / 1000);
     setTimeStamp(generateStamp);
     setHash(
       md5(
-        `${generateStamp}abd7413b31d0e6b498b980639e4f3a04a4b29402b20c16f1ac80cb70cecd2ee43e396aaa`
+        `${generateStamp}f97429388fb6981ed372948b48815a062ae8588b7fda74fc35c2561abc4a43fc02eda15e`
       )
     );
   }, []);
 
-  const GetCharacters = async () => {
+  const GetCharacters = async (timeStamp, hash) => {
     const responseChar = await API.get(
-      `characters?ts=${timeStamp}&apikey=${apiKey}&hash=${hash}`
+      `characters?ts=${timeStamp}&apikey=${apiKey}&hash=${hash}&limit=60`
     );
-    setCharacters(responseChar);
+    setCharacters(responseChar.data.data.results);
+    console.log(responseChar.data.data.results);
   };
 
-  const GetComics = async () => {
+  const GetComics = async (timeStamp, hash) => {
     const responseComics = await API.get(
       `comics?ts=${timeStamp}&apikey=${apiKey}&hash=${hash}`
     );
     setComics(responseComics.data.data.results);
   };
+
+  useEffect(() => {
+    if (timeStamp && hash) {
+      GetCharacters(timeStamp, hash);
+      GetComics(timeStamp, hash);
+    }
+  }, [timeStamp, hash]);
 
   const value = useMemo(() => ({
     comics,
